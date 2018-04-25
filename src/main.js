5e8fcdb9ded037e7002f20a70bdb60f64e969589
russellomo <omo.russell@gmail.com>
2018-04-24 21:51:19 -1000

    basic login and account creation

--- src/main.js
+++ src/main.js
@@ -3,13 +3,30 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import firebase from 'firebase';
 
 Vue.config.productionTip = false;
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>',
+let app;
+// Development group's config for firebase, change if necessary for own back end.
+var config = {
+  apiKey: "AIzaSyAZMTKNzIKi3g5vxSOHy6OcKjbUntfcMAU",
+  authDomain: "music-app-465.firebaseapp.com",
+  databaseURL: "https://music-app-465.firebaseio.com",
+  projectId: "music-app-465",
+  storageBucket: "",
+  messagingSenderId: "970280687612"
+};
+firebase.initializeApp(config);
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (!app) {
+    /* eslint-disable no-new */
+    app = new Vue({
+      el: '#app',
+      template: '<App/>',
+      components: { App },
+      router
+    })
+  }
 });
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,51 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import Vue from 'vue'
+import Router from 'vue-router'
 
-Vue.use(Router);
+import Hello from '@/components/HelloWorld'
+import Login from '@/components/login'
+import SignUp from '@/components/sign-up'
+import firebase from 'firebase'
 
-export default new Router({
+Vue.use(Router)
+
+let router = new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      path: '*',
+      redirect: '/login'
     },
-  ],
-});
+    {
+      path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/sign-up',
+      name: 'SignUp',
+      component: SignUp
+    },
+    {
+      path: '/hello',
+      name: 'Hello',
+      component: Hello,
+      meta: {
+        requiresAuth: true
+      }
+    }
+  ]
+})
+
+router.beforeEach((to, from, next) => {
+  let currentUser = firebase.auth().currentUser;
+  let requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+
+  if (requiresAuth && !currentUser) next('login')
+  else if (!requiresAuth && currentUser) next('hello')
+  else next()
+})
+
+export default router
